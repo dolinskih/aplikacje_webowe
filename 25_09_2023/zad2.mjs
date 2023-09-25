97bfde46d6039b15ec37313c47446c1c456b87b7
dolinskih <047hubert@gmail.com>
2023-09-25 15:27:57 +0200

    Lekcja 2, 25.09.2023

--- 25_09_2023/zad2.mjs
+++ 25_09_2023/zad2.mjs
@@ -1,15 +1,14 @@
 import http from 'http'
-import {readFile} from 'fs'
+import {readFile} from 'fs/promises'
 const host = '127.0.0.1'
 const port = 3000
 
 const server = http.createServer(async (req, res)=>{
-    await readFile('./index.html', function(err, data){
-        res.statusCode = 200
-        res.setHeader('content-type', 'text/html')
-        res.write(data)
-        res.end()
-    })
+    const html = await readFile('./index.html')
+    res.statusCode = 200
+    res.setHeader('content-type', 'text/html')
+    res.write(html)
+    res.end()
 })
 
 server.listen(port, host, ()=>{
