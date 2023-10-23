a93270d9d25182be1ae08653baed6bea7e2431c5
dolinskih <047hubert@gmail.com>
2023-10-23 13:34:48 +0200

    Lekcja 23.10.2023

---
+++ 23_10_2023/index.mjs
@@ -0,0 +1,193 @@
+import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url'
+import bodyParser from 'body-parser'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const app = express()
+const port = 3000
+
+const students = [
+    {
+        id: 1,
+        name: "Jan",
+        surname: "Kowalski",
+        email: "jan.kowalski@email.pl"
+    },
+    {
+        id: 2,
+        name: "Piotr",
+        surname: "Nowak",
+        email: "piotr.nowak@email.pl"
+    },
+    {
+        id: 3,
+        name: "Ryszard",
+        surname: "Styrta",
+        email: "ryszard.styrta@email.pl"
+    },
+    {
+        id: 4,
+        name: "Tomasz",
+        surname: "Bączek",
+        email: "tomasz.baczek@email.pl"
+    },
+    {
+        id: 5,
+        name: "Dżesika",
+        surname: "Tramp",
+        email: "dzesika.tramp@email.com"
+    },
+    {
+        id: 6,
+        name: "Jan",
+        surname: "Paweł",
+        email: "jan.pawel@email.pl"
+    },
+    {
+        id: 7,
+        name: "Krzysztof",
+        surname: "Kochański",
+        email: "krzysztof.kochanski@email.pl"
+    },
+    {
+        id: 8, 
+        name: "Bronisław",
+        surname: "Stary",
+        email: "bronislaw.stary@email.pl"
+    },
+    {
+        id: 9,
+        name: "Karol",
+        surname: "Lidlowy",
+        email: "karol.lidlowy@email.pl"
+    },
+    {
+        id: 10,
+        name: "Nataniel",
+        surname: "Aureliusz",
+        email: "nataniel.aureliusz@email.pl"
+    }
+]
+
+const subjects = [
+    {
+        id: 1,
+        subject: "Język polski",
+        hoursAWeek: 5
+    },
+    {
+        id: 2,
+        subject: "Matematyka",
+        hoursAWeek: 3
+    },
+    {
+        id: 3,
+        subject: "Język angielski",
+        hoursAWeek: 8
+    },
+    {
+        id: 4,
+        subject: "Aplikacje mobilne",
+        hoursAWeek: 2
+    },
+    {
+        id: 5,
+        subject: "Geografia",
+        hoursAWeek: 25
+    },
+    {
+        id: 6,
+        subject: "Biologia",
+        hoursAWeek: -3
+    },
+    {
+        id: 7,
+        subject: "Systemy komputrowe JS",
+        hoursAWeek: 12
+    },
+    {
+        id: 8,
+        subject: "Historia",
+        hoursAWeek: 10
+    },
+    {
+        id: 9,
+        subject: "Wiedza o społeczeństwie",
+        hoursAWeek: 1
+    },
+    {
+        id: 10,
+        subject: "Chemia",
+        hoursAWeek: 13
+    }
+]
+
+const notFoundError = {
+    "Error": 404,
+    "Message": "Brak danych pod tym adresem."
+}
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/kontakt', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/kontakt.html'))
+})
+
+app.post('/kontakt', (req, res)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.get('/api', (req, res)=>{
+    const apiInfo = {
+        "/api" : "Informacje o adresach url API.",
+        "/api/students": "Lista studentów z ich informacjami.",
+        "/api/students/:id": "Informacje o wybranym studencie wg id.",
+        "/api/subjects": "Lista przedmiotów szkolnych.",
+        "/api/subjects/:id": "Informacje o wybranym przedmiocie wg id."
+    }
+    res.json(apiInfo)
+})
+
+app.get('/api/students', (req, res)=>{
+    res.json(students)
+})
+
+app.get('/api/students/:id', (req, res)=>{
+    const chosenStudentId = req.params.id
+    const chosenStudent = students.find(student => student.id == chosenStudentId)
+    if(chosenStudent){
+        res.json(chosenStudent)
+    }
+    else{
+        res.status(404).send(notFoundError)
+    }
+})
+
+app.get('/api/subjects', (req, res)=>{
+    res.json(subjects)
+})
+
+app.get('/api/subjects/:id', (req, res)=>{
+    const chosenSubjectId = req.params.id
+    const chosenSubject = subjects.find(subject => subject.id == chosenSubjectId)
+    if(chosenSubject){
+        res.json(chosenSubject)
+    }
+    else{
+        res.status(404).send(notFoundError)
+    }
+})
+
+app.listen(port, ()=>{
+    console.log(`App running at http://localhost:${port}`)
+})
