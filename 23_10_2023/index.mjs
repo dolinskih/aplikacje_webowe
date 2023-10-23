e9a8ad11c065b741578715e889dea81fb87a5bb9
dolinskih <047hubert@gmail.com>
2023-10-23 13:58:56 +0200

    Lekcja 23.10.2023

--- 23_10_2023/index.mjs
+++ 23_10_2023/index.mjs
@@ -27,47 +27,6 @@ app.post('/kontakt', (req, res)=>{
     res.redirect('/')
 })
 
-// app.get('/api', (req, res)=>{
-//     const apiInfo = {
-//         "/api" : "Informacje o adresach url API.",
-//         "/api/students": "Lista studentów z ich informacjami.",
-//         "/api/students/:id": "Informacje o wybranym studencie wg id.",
-//         "/api/subjects": "Lista przedmiotów szkolnych.",
-//         "/api/subjects/:id": "Informacje o wybranym przedmiocie wg id."
-//     }
-//     res.json(apiInfo)
-// })
-
-// app.get('/api/students', (req, res)=>{
-//     res.json(students)
-// })
-
-// app.get('/api/students/:id', (req, res)=>{
-//     const chosenStudentId = req.params.id
-//     const chosenStudent = students.find(student => student.id == chosenStudentId)
-//     if(chosenStudent){
-//         res.json(chosenStudent)
-//     }
-//     else{
-//         res.status(404).send(notFoundError)
-//     }
-// })
-
-// app.get('/api/subjects', (req, res)=>{
-//     res.json(subjects)
-// })
-
-// app.get('/api/subjects/:id', (req, res)=>{
-//     const chosenSubjectId = req.params.id
-//     const chosenSubject = subjects.find(subject => subject.id == chosenSubjectId)
-//     if(chosenSubject){
-//         res.json(chosenSubject)
-//     }
-//     else{
-//         res.status(404).send(notFoundError)
-//     }
-// })
-
 app.listen(port, ()=>{
     console.log(`App running at http://localhost:${port}`)
 })
