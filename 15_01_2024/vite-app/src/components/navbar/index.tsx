ca3fa034102f38705aa49b693ba96f385d65a06f
dolinskih <047hubert@gmail.com>
2024-01-15 13:18:05 +0100

    Lekcja 15.01.2024

---
+++ 15_01_2024/vite-app/src/components/navbar/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import { routes } from "../../helpers/routing"
+
+function Navbar(){
+    return (
+        <div>
+            <nav>
+                <ul>
+                    {routes.map((route)=>(
+                        <li key={route.path}>
+                            <Link to={route.path}>{route.label}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ 15_01_2024/vite-app/src/pages/contact/index.tsx
@@ -0,0 +1,13 @@
+import Heading from "../../components/heading"
+
+function Contact(){
+    return (
+        <div>
+            <Heading
+                title="Contact"
+            />
+        </div>
+    )
+}
+
+export default Contact
